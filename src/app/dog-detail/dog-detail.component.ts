9c6260e65de839aae0dc14a8a074dbedafb8b13d
Daria Deriagina <w0862447@myscc.ca>
2024-10-15 15:42:01 -0400

    add dog detail component html and import in ts file

--- src/app/dog-detail/dog-detail.component.ts
+++ src/app/dog-detail/dog-detail.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {Pet} from '../Shared/pet';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-dog-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './dog-detail.component.html',
   styleUrl: './dog-detail.component.css'
 })
