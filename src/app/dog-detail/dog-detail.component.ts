13f2863f24fa37a9becae55667f166ede8b8d671
Daria Deriagina <w0862447@myscc.ca>
2024-10-16 16:35:22 -0400

    dog detail component ts filled

--- src/app/dog-detail/dog-detail.component.ts
+++ src/app/dog-detail/dog-detail.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Pet} from '../Shared/pet';
 import {NgIf} from '@angular/common';
+import {ActivatedRoute, Router} from '@angular/router';
+import {DogService} from '../Services/dog.service';
 
 @Component({
   selector: 'app-dog-detail',
@@ -9,7 +11,39 @@ import {NgIf} from '@angular/common';
   templateUrl: './dog-detail.component.html',
   styleUrl: './dog-detail.component.css'
 })
-export class DogDetailComponent {
-  @Input() dog? :Pet;
+export class DogDetailComponent implements OnInit{
+  // I need variable for a do to be displayed, for whole array....
+  // declare three variables
+
+  dog:Pet | undefined;
+  dogList:Pet[]=[]
+  currentIndex:number=0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dogService:DogService
+  ) {
+  }
+
+  ngOnInit(){
+    this.dogService.getDog().subscribe(dogs=>{
+      this.dogList = dogs
+
+      this.route.paramMap.subscribe(params=>{
+        const id = Number(params.get('id'));
+        if(id){
+          this.currentIndex=this.dogList.findIndex(dog=>dog.id===id);
+          this.dog=this.dogList[this.currentIndex];
+        }
+      })
+    })
+
+  }
+
+
+
+  // @Input() dog? :Pet;
 
 }
+
